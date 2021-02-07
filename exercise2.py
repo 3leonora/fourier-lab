8baf544e9c2e629d8da86543bd920ff94fc81ff5
Eleonora Svanberg <eleonora.svanberg@gmail.com>
2021-02-07 15:07:17 +0100

    fixed some issues but one left

--- exercise2.py
+++ exercise2.py
@@ -23,14 +23,13 @@ def sample(T1:int) -> np.ndarray:
     '''
     N = 1024 # number of data points
     T0 = 0.0 # start of sample [s]
-    T1 = 0.05 # start of sample [s]
     t = np.linspace(T0, T1, N, endpoint=False) # array with sample time
     Ts = T1-T0
     deltat = Ts/N
     vs = 1/deltat
     deltav = 1/Ts
-    print(f't[0]={t[0]}, t[1]={t[1]}, t[2]={t[2]}')
-    print(f'Ts={Ts}, N={N}, deltat={deltat}, vs={vs}, deltav={deltav}')
+    print(f't[0]={t[0]:.2f}, t[1]={t[1]:.2f}, t[2]={t[2]:.2f}')
+    print(f'Ts={Ts}, N={N}, deltat={deltat:.2f}, vs={vs:.2f}, deltav={deltav:.2f}')
 
     return t
 
@@ -47,7 +46,7 @@ ny = 20.0 # frequency [Hz]
 omega = 2*np.pi*ny # angular frequency [rad]
 ft1 = f(t1, omega) # signal f(t)
 ft2 = f(t2, omega)
-T = np.pi*2/omega
+T = 2*np.pi/omega
 print(f'T={T} s')
 
 # c) Plot graph
@@ -59,23 +58,25 @@ plt.show()
 # d) Fourier Transform
 Fft1 = np.fft.rfft(ft1) # Fouriertransform F(f) of real signal f(t)
 plt.plot(np.real(Fft1),'o', color='g', label='ft1') # Plot real part of Fft vs looping index 
-plt.plot(np.imag(Fft1),'o', color='g') # Plot imaginary part of Fft vs looping index 
+plt.plot(np.imag(Fft1),'o', color='r') # Plot imaginary part of Fft vs looping index 
 Fft2 = np.fft.rfft(ft2) # Fouriertransform F(f) of real signal f(t)
-plt.plot(np.real(Fft2),'o', color='b', label='ft2') # Plot real part of Fft vs looping index 
-plt.plot(np.imag(Fft2),'o', color='b') # Plot imaginary part of Fft vs looping index 
+plt.plot(np.real(Fft2),'x', color='g', label='ft2') # Plot real part of Fft vs looping index 
+plt.plot(np.imag(Fft2),'x', color='r') # Plot imaginary part of Fft vs looping index 
 plt.legend()
 plt.show()
 
 # e) If T1=0.1, what happens then?
 
 # f) Correction of sample time
-ny_max1 = N/(2.*(T11-T0))
-ny_max2 = N/(2.*(T12-T0))
+N = 1024
+T0 = 0.0
+ny_max1 = N/(2*(T11-T0))
+ny_max2 = N/(2*(T12-T0))
 frek1 = np.linspace(0, ny_max1, N/2+1, endpoint=True)
 frek2 = np.linspace(0, ny_max2, N/2+1, endpoint=True)
-plt.plot(frek1,np.real(Fft1),'o', color='g', label='ft1') # plotta realdelen av Fft mot frek
-plt.plot(frek1,np.imag(Fft1),'o', color='g') # plotta imaginardelen mot frek
-plt.plot(frek2,np.real(Fft2),'o', color='b', label='ft2') # plotta realdelen av Fft mot frek
-plt.plot(frek2,np.imag(Fft2),'o', color='b') # plotta imaginardelen mot frek
+plt.plot(frek1,np.real(Fft1),'o', color='g', label='ft1') # Plot real part of Fft vs frek 
+plt.plot(frek1,np.imag(Fft1),'o', color='r') # Plot imaginary part of Fft vs frek 
+plt.plot(frek2,np.real(Fft2),'x', color='g', label='ft2')
+plt.plot(frek2,np.imag(Fft2),'x', color='r') 
 plt.legend()
 plt.show()
